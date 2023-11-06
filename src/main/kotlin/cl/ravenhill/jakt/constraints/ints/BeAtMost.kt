bcbcbb2f7c30c4b5cd9a6bc94070d378454b0481
R8V <ignacio.slater@ug.uchile.cl>
2023-11-06 13:15:47 -0300

    Added tests and improved documentation for BeAtMost constraint
    Implemented unit tests for the BeAtMost class in BeAtMostTest which verifies the validator's successful and failed scenarios. Also, enhanced BeAtMost class documentation by providing a usage example and a more comprehensive explanation about how it works. This improvement will support other developers in understanding its function easily.

--- src/main/kotlin/cl/ravenhill/jakt/constraints/ints/BeAtMost.kt
+++ src/main/kotlin/cl/ravenhill/jakt/constraints/ints/BeAtMost.kt
@@ -1,9 +1,24 @@
 package cl.ravenhill.jakt.constraints.ints
 
 /**
- * Represents a requirement that an integer value must be at most a specified value.
+ * Represents a constraint that checks if a given [Int] value is less than or equal to a specified maximum value.
  *
- * @param maxInclusive The maximum allowed value.
- * @property most The maximum allowed value.
+ * This class is a specialization of [BeInRange], where the range is defined from the minimum integer value up to the
+ * provided maximum value (inclusive).
+ * In essence, it checks if a given integer value lies in the range `[Int.MIN_VALUE, maxInclusive]`.
+ *
+ * ## Usage
+ * ### Example: Checking if an integer is at most a certain value
+ * ```kotlin
+ * val constraint = BeAtMost(10)
+ * val success = constraint.validator(5) // This will return `true` since 5 is less than or equal to 10
+ * val failure = constraint.validator(15) // This will return `false` since 15 is greater than 10
+ * ```
+ *
+ * @param maxInclusive The maximum value (inclusive) that an integer should not exceed to satisfy the constraint.
+ *
+ * @author <a href="https://www.github.com/r8vnhill">Ignacio Slater M.</a>
+ * @since 1.0.0
+ * @version 1.0.0
  */
 class BeAtMost(maxInclusive: Int) : BeInRange(Int.MIN_VALUE..maxInclusive)
