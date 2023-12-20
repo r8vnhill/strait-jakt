358fcf1436640aa42af632936a40ceca7b2d662d
R8V <ignacio.slater@ug.uchile.cl>
2023-12-20 18:35:22 -0300

    Update dependencies and refactor tests in Jakt project
    This commit updates several dependencies to their newer versions and modifies a test setup in the Jakt project. Specifically, the Kotest version for assertions, properties, and junit5 runner, along with the Dokka and Detekt plugins, have been updated. The usage of these dependencies in the respective config files has been adjusted accordingly. A JaktTest has also been refactored to opt into ExperimentalKotest and to modify the number of iterations within checkAll in a test case.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 val kotestVersion = extra["kotest.version"] as String
 val dokkaVersion = extra["dokka.version"] as String
 val detektVersion = extra["detekt.version"] as String
 
 plugins {
-    kotlin("jvm")
+    `maven-publish`
     id("io.gitlab.arturbosch.detekt")
     id("org.jetbrains.dokka")
-    `maven-publish`
+    kotlin("jvm")
     signing
-    `java-library`
 }
 
 group = "cl.ravenhill"
-version = "1.2.1"
+version = "1.3.0"
 val projectVersion = version.toString()
 
 repositories {
@@ -32,6 +33,22 @@ tasks.test {
     useJUnitPlatform()
 }
 
+tasks.withType<DokkaTask>().configureEach {
+    outputDirectory.set(layout.buildDirectory.dir("dokka/html"))
+}
+
+val dokkaHtml by tasks.getting(DokkaTask::class)
+
+val dokkaJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
 kotlin {
     jvmToolchain(17)
 }
@@ -41,45 +58,68 @@ detekt {
     config.from(files("conf/detekt.yml"))
 }
 
-java {
-    // Generates a Javadoc jar file containing the Javadoc for this project's public API
-    withJavadocJar()
-    // Generates a sources jar file containing the sources for this project
-    withSourcesJar()
-}
-
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            from(components["java"])
-            groupId = "cl.ravenhill"
+            groupId = group as String
             artifactId = "strait-jakt"
-            description = "A Kotlin library for validating values."
-        }
-        withType<MavenPublication> {
+            description = "A Kotlin data validation library."
+            from(components["kotlin"])
+            artifact(dokkaJar)
+            artifact(sourcesJar)
             pom {
-                packaging = "jar"
-                name.set("strait-jakt")
-                description.set("StraitJackt")
+                name.set("StraitJakt")
+                description.set("A Kotlin data validation library.")
                 url.set("https://github.com/r8vnhill/strait-jakt")
-                inceptionYear.set("2023")
                 licenses {
                     license {
                         name.set("BSD 2-Clause License")
                         url.set("https://opensource.org/licenses/BSD-2-Clause")
                     }
                 }
+                scm {
+                    url.set("https://github.com/r8vnhill/strait-jakt")
+                    connection.set("scm:git:git://github.com/r8vnhill/strait-jakt.git")
+                    developerConnection.set("scm:git:ssh://github.com/r8vnhill/strait-jakt.git")
+                }
                 developers {
                     developer {
                         id.set("r8vnhill")
                         name.set("Ignacio Slater M.")
-                        email.set("reachme@gmail.com")
+                        email.set("reachme@ravenhill.cl")
                     }
                 }
-                scm {
-                    connection.set("scm:git:git@github.com:r8vnhill/strait-jakt.git")
-                    developerConnection.set("scm:git:ssh:git@github.com:r8vnhill/strait-jakt.git")
-                    url.set("https://github.com/r8vnhill/strait-jakt")
+            }
+        }
+    }
+
+    if (!projectVersion.endsWith("SNAPSHOT")) {
+        signing {
+            useGpgCmd()
+            sign(publications)
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = if (projectVersion.endsWith("SNAPSHOT")) {
+                uri("https://oss.sonatype.org/content/repositories/snapshots")
+            } else {
+                uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            }
+            credentials {
+                username = if (System.getProperty("os.name").startsWith("Windows")) {
+                    System.getenv("SonatypeUsername")
+                } else {
+                    System.getProperty("SONATYPE_USERNAME")
+                }.apply {
+                    check(!isNullOrEmpty()) { "Sonatype username not found." }
+                    password = if (System.getProperty("os.name").startsWith("Windows")) {
+                        System.getenv("SonatypePassword")
+                    } else {
+                        System.getProperty("SONATYPE_PASSWORD")
+                    }.apply { check(!isNullOrEmpty()) { "Sonatype password not found." } }
                 }
             }
         }
