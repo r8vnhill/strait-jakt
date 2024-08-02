f9a6867fa1b6cee430b029eaf9d84c6ea89aab9e
r8vnhill <reachme@ravenhill.cl>
2024-08-02 17:30:31 -0400

    📝 Add NOTICE file and configure Maven publication
    - Added Apache 2.0 license NOTICE file for compliance.
    - Introduced new utility classes for publishing.
    - Enhanced Maven publication configuration with improved signing, deployment to Maven Central, and environment-based credentials handling.

--- convention-plugins/src/main/kotlin/module.publication.gradle.kts
+++ convention-plugins/src/main/kotlin/module.publication.gradle.kts
@@ -3,30 +3,78 @@
  * 2-Clause BSD License.
  */
 
-import org.gradle.api.publish.maven.MavenPublication
-import org.gradle.api.tasks.bundling.Jar
-import org.gradle.kotlin.dsl.`maven-publish`
-import kotlin.jvm.optionals.getOrElse
-
-val versionCatalog: VersionCatalog = versionCatalogs.named("libs")
-val jaktVersion: String =
-    versionCatalog.findVersion("jakt").getOrElse { error("Version for 'jakt' not found") }.requiredVersion
+import utils.ReleaseVersion
 
 plugins {
+    id("jakt-base")
     `maven-publish`
     signing
+    id("dev.adamko.dev-publish")
+}
+
+val ossrhUsername: String by project
+val ossrhPassword: String by project
+val signingKey: String? by project
+val signingPassword: String? by project
+
+val releaseVersion = ReleaseVersion(versionCatalogs.named("libs"))
+
+val mavenCentralRepoName = "Deploy"
+
+signing {
+    // Check if the signing key and signing password are not null or blank.
+    // If both are provided, configure the signing process.
+    if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+        // Use the GPG command line tool for signing.
+        useGpgCmd()
+        // Use the provided signing key and password for in-memory PGP keys.
+        useInMemoryPgpKeys(signingKey, signingPassword)
+    }
+    // Sign all publications defined in the 'publishing' block.
+    sign(publishing.publications)
+    // Set the signing requirement based on whether it is a release version.
+    // Signing is only required when the version is a release version.
+    setRequired { releaseVersion.isRelease }
+}
+
+gradle.taskGraph.whenReady {
+    // Determine if any of the tasks in the task graph are publishing to Maven Central.
+    val isPublishingToMavenCentral = allTasks
+        // Filter tasks that are instances of PublishToMavenRepository.
+        .filterIsInstance<PublishToMavenRepository>()
+        // Check if any of these tasks are targeting the Maven Central repository.
+        .any { it.repository?.name == mavenCentralRepoName }
+
+    // Set the signing requirement based on whether publishing to Maven Central is occurring.
+    signing.setRequired({ isPublishingToMavenCentral })
+
+    // Configure all Sign tasks.
+    tasks.withType<Sign>().configureEach {
+        // Redefine the variable for Config Cache compatibility.
+        val isPublishingToMavenCentralRedefined = isPublishingToMavenCentral
+
+        // Add the isPublishingToMavenCentral property to the inputs for Config Cache compatibility.
+        inputs.property("isPublishingToMavenCentral", isPublishingToMavenCentralRedefined)
+
+        // Configure the Sign tasks to only run if publishing to Maven Central.
+        onlyIf("publishing to Maven Central") { isPublishingToMavenCentralRedefined }
+    }
 }
 
 publishing {
-    // Configure all publications
-    publications.withType<MavenPublication> {
-        // Stub javadoc.jar artifact
-        artifact(tasks.register("${name}JavadocJar", Jar::class) {
-            archiveClassifier.set("javadoc")
-            archiveAppendix.set(this@withType.name)
-        })
-
-        // Provide artifacts information required by Maven Central
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            name = mavenCentralRepoName
+            url = if (releaseVersion.isRelease) releasesRepoUrl else snapshotsRepoUrl
+            credentials {
+                username = System.getenv("OSSRH_USERNAME") ?: ossrhUsername
+                password = System.getenv("OSSRH_PASSWORD") ?: ossrhPassword
+            }
+        }
+    }
+    publications.withType<MavenPublication>().configureEach {
         pom {
             name.set("StraitJakt")
             description.set("A Kotlin data validation library.")
@@ -52,41 +100,18 @@ publishing {
             }
         }
     }
-
-    if (!jaktVersion.endsWith("SNAPSHOT")) {
-        signing {
-            useGpgCmd()
-            sign(publications)
-        }
-    }
-
-    repositories {
-        maven {
-            name = "OSSRH"
-            url = if (project.version.toString().endsWith("SNAPSHOT")) {
-                uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-            } else {
-                uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            }
-            credentials {
-                username = if (System.getProperty("os.name").startsWith("Windows")) {
-                    System.getenv("SonatypeUsername")
-                } else {
-                    System.getenv("SONATYPE_USERNAME")
-                }.apply { check(!isNullOrEmpty()) { "Sonatype username not found." } }
-                password = if (System.getProperty("os.name").startsWith("Windows")) {
-                    System.getenv("SonatypePassword")
-                } else {
-                    System.getenv("SONATYPE_PASSWORD")
-                }.apply { check(!isNullOrEmpty()) { "Sonatype password not found." } }
-            }
-        }
-    }
 }
 
-signing {
-    if (project.hasProperty("signing.gnupg.keyName")) {
-        useGpgCmd()
-        sign(publishing.publications)
+pluginManager.withPlugin("org.jetbrains.kotlin.multiplatform") {
+    val javadocJar by tasks.registering(Jar::class) {
+        group = JavaBasePlugin.DOCUMENTATION_GROUP
+        description = "Create Javadoc JAR"
+        archiveClassifier.set("javadoc")
     }
+
+    publishing.publications.withType<MavenPublication>().configureEach {
+        artifact(javadocJar)
+    }
+
+    
 }
